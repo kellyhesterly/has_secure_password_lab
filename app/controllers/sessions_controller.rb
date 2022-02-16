acd837c436192f86b72f31af0575b385568496fa
github username <kelly.hesterly@gmail.com>
2022-02-15 23:00:36 -0700

    commit tree 9f7cc95fd9886ba5f60cdc8415f9520993205255

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,9 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(name: params[:user][:name])
 
-        user = user.try(:authenticate, params[:user][:password])
+        authenticate = user.try(:authenticate, params[:user][:password])
     
-        return redirect_to(controller: 'sessions', action: 'new') unless user
+        return redirect_to(controller: 'sessions', action: 'new') unless authenticate
     
         session[:user_id] = user.id
     
